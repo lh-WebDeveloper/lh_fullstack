47e68bb06b8d4fec7755a13b3980c482df75d450
lh19981109 <2727079972@qq.com>
2021-01-21 14:56:32 +0800

    replace Array.from to...

--- git_practice/unique.js
+++ git_practice/unique.js
@@ -2,7 +2,8 @@
 const unique = (arr) => {
         //1. 伪数组 nodeList
         //2. Set
-        return  Array.from(new Set(arr))
+        //return  Array.from(new Set(arr))
+	return [...new Set(arr)];
 }
 
 console.log(unique([1,2,2,3]))
