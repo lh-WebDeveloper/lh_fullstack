691ec76da3a1e840c9f1f8a67b66ca668d64660d
lh19981109 <2727079972@qq.com>
2021-01-23 20:37:14 +0800

    变量

---
+++ JavaScript高级程序设计/第3章/3.3变量/const.js
@@ -0,0 +1,19 @@
+/* const name= "li";
+if(true){
+    const name ="hi";
+}
+console.log(name);
+
+let i =0;
+for(const j = 7;  i < 5; ++i){
+    console.log(j);
+}
+for(const key in {a:1,b:2}){
+    console.log(key);
+}
+for(const value of[1,2,3,4,5]){
+    console.log(value);
+}
+ */
+const age = 26;
+age = 36// TypeError
