d08ded1db12e7f45ac44b8c6724ac3fe8f972421
lh19981109 <2727079972@qq.com>
2021-02-04 21:01:46 +0800

    echarts

---
+++ wxapp/echarts/README.md
@@ -0,0 +1,10 @@
+# swan hello world
+
+开发者工具新建工程,生成demo
+
+- 图形化有利于大数据的可视化
+- 引入echart 库
+    <chart option="{{option}}"/>
+    1. legend 说明
+    2. grid 网格
+    3. xAxis 横轴 yAxis 纵轴
---
+++ wxapp/echarts/pages/index/index.js
@@ -0,0 +1,20 @@
+Page({
+    data: {
+        charts: [{
+            id: 'bar',
+            name: '柱状图'
+        }, {
+            id: 'pie',
+            name: '饼图'
+        }, {
+            id: 'line',
+            name: '折线图'
+        }]
+    },
+    showChart(e) {
+        const id = e.currentTarget.dataset.id;
+        swan.navigateTo({
+            url: '/pages/' + id + '/' + id
+        });
+    }
+})
