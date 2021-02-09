b918be0653b5e2b5aecba8ffb7751f31d49e98e6
lh19981109 <2727079972@qq.com>
2021-02-09 18:55:56 +0800

    折线图

---
+++ wxapp/echarts/pages/line/line.js
@@ -0,0 +1,122 @@
+const option = {
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'cross',
+            label: {
+                backgroundColor: '#6a7985'
+            }
+        }
+    },
+    legend: {
+        data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {}
+        }
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    xAxis: [
+        {
+            type: 'category',
+            boundaryGap: false,
+            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        }
+    ],
+    yAxis: [
+        {
+            type: 'value'
+        }
+    ],
+    series: [
+        {
+            name: '邮件营销',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            emphasis: {
+                focus: 'series'
+            },
+            data: [120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+            name: '联盟广告',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            emphasis: {
+                focus: 'series'
+            },
+            data: [220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+            name: '视频广告',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            emphasis: {
+                focus: 'series'
+            },
+            data: [150, 232, 201, 154, 190, 330, 410]
+        },
+        {
+            name: '直接访问',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            emphasis: {
+                focus: 'series'
+            },
+            data: [320, 332, 301, 334, 390, 330, 320]
+        },
+        {
+            name: '搜索引擎',
+            type: 'line',
+            stack: '总量',
+            label: {
+                show: true,
+                position: 'top'
+            },
+            areaStyle: {},
+            emphasis: {
+                focus: 'series'
+            },
+            data: [820, 932, 901, 934, 1290, 1330, 1320]
+        }
+    ]
+};
+Page({
+    data: {
+        option:option,
+    },
+    onLoad: function () {
+        // 监听页面加载的生命周期函数
+    },
+    onReady: function() {
+        // 监听页面初次渲染完成的生命周期函数
+    },
+    onShow: function() {
+        // 监听页面显示的生命周期函数
+    },
+    onHide: function() {
+        // 监听页面隐藏的生命周期函数
+    },
+    onUnload: function() {
+        // 监听页面卸载的生命周期函数
+    },
+    onPullDownRefresh: function() {
+        // 监听用户下拉动作
+    },
+    onReachBottom: function() {
+        // 页面上拉触底事件的处理函数
+    },
+    onShareAppMessage: function () {
+        // 用户点击右上角转发
+    }
+});
