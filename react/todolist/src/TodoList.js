0e39ad9264b206894a72a0eea94f7a23826da296
lh19981109 <2727079972@qq.com>
2021-03-06 16:05:20 +0800

    todolist

---
+++ react/todolist/src/TodoList.js
@@ -0,0 +1,52 @@
+import React,{Component,Fragment} from 'react';
+import TodoItem from './TodoItem';
+import './style.css'
+class TodoList extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            list:[],
+            inputValue:''
+        }
+        this.handleInputChange=this.handleInputChange.bind(this);
+        this.handleBtnClick=this.handleBtnClick.bind(this);
+        this.handleDelete=this.handleDelete.bind(this);
+    }
+    handleBtnClick(){
+        this.setState({
+            list:[...this.state.list,this.state.inputValue],
+            inputValue:''
+        })
+    }
+    handleInputChange(e){
+        this.setState({
+            inputValue:e.target.value
+        })
+    }
+    //父组件通过属性的形式向子组件传递参数
+    //子组件通过props接受父组件传递过来的参数
+    handleDelete(index){
+        const list =[...this.state.list];
+        list.splice(index,1);
+        this.setState({list})
+    }
+    getTodoItems(){
+        return(
+            this.state.list.map((item,index)=>{
+                return (<TodoItem delete={this.handleDelete} key={index} content={item} index={index}/>);
+            })
+        )
+    }
+    render() { 
+        return ( 
+            <Fragment>
+            <div>
+                <input value={this.state.inputValue} onChange={this.handleInputChange}/>
+                <button className='red-btn' onClick={this.handleBtnClick}>add</button>
+            </div>
+            <ul>{this.getTodoItems()}</ul>
+            </Fragment>
+         );
+    }
+}
+export default TodoList;
