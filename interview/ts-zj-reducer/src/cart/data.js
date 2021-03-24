56b794e4f8107f82321d649922d2acc81e0c0c46
lh19981109 <2727079972@qq.com>
2021-03-24 16:56:22 +0800

    reducer购物车

---
+++ interview/ts-zj-reducer/src/cart/data.js
@@ -0,0 +1,11 @@
+const cartData = Array(5)
+    .fill(undefined)
+    .map((e,index)=>(
+        {
+            id:index,
+            name:`商品${index}`,
+            price:Math.round(Math.random()*100)
+        }
+    ))
+console.log(cartData);
+export default cartData;
