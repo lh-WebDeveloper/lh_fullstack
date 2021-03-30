c35691f8695713d7c7eb6d027f1b0a46c0b52da3
lh19981109 <2727079972@qq.com>
2021-03-30 16:45:15 +0800

    music

---
+++ react/react-cloud-music-chapter2/README.md
@@ -0,0 +1,11 @@
+# 从零开始开发react项目
+    1. 单页应用,路由接管一切
+        1.1 react-router-config
+        1.2 建好目录
+            /routes/index.js 数组
+        1.3 父子路由,父子组件 rendr
+        当前路由当前组件带来动态加载效果
+    2. Home layout 
+        2.1 头部静态界面,styled-components 提供了样式组件容器
+        界面开发 html(props.children)+css(代码) 功能设计
+        2.2 原生支持嵌套 变量定义 函数 stylus 可以不学了
---
+++ react/react-cloud-music-chapter2/src/routes/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Redirect} from 'react-router-dom';//->home
+import Home from '../pages/Home';
+import Recommend from '../pages/Recommend';
+import Singers from '../pages/Singers';
+import Rank from '../pages/Rank';
+export default[
+    {
+        path:'/',
+        component:Home,
+        routes:[
+            {
+                path:'/',
+                exact:true,
+                render:()=>(
+                    <Redirect to={"/recommend"}/>
+                )
+            },
+            {
+                path:'/recommend',
+                component:Recommend
+            },
+            {
+                path:'/singers',
+                component:Singers
+            },
+            {
+                path:'/rank',
+                component:Rank
+            }
+        ]
+    }
+]
