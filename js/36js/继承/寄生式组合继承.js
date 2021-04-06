b838fb6c17e499d0d8f7bcf476abbcbd0b1c3427
lh19981109 <2727079972@qq.com>
2021-04-06 14:25:07 +0800

    继承

---
+++ js/36js/继承/寄生式组合继承.js
@@ -0,0 +1,11 @@
+function object(o) {
+    function F() {}
+    F.prototype = o
+    return new F()
+}
+function inheritPrototype(child, parent) {
+    let prototype = object(parent.prototype)
+    prototype.constructor = child
+    child.prototype = prototype
+}
+inheritPrototype(Dog, Animal)
---
+++ js/36js/继承/组合继承.js
@@ -0,0 +1,23 @@
+function Animal(name) {
+    this.name = name
+    this.colors = ['black', 'white']
+}
+Animal.prototype.getName = function() {
+    return this.name
+}
+function Dog(name, age) {
+    Animal.call(this, name)
+    this.age = age
+}
+Dog.prototype =  new Animal()
+Dog.prototype.constructor = Dog
+
+let dog1 = new Dog('奶昔', 2)
+dog1.colors.push('brown')
+let dog2 = new Dog('哈赤', 1)
+console.log(dog2) 
+// { name: "哈赤", colors: ["black", "white"], age: 1 }
+
+// 组合继承已经相对完善了，但还是存在问题，它的问题就是调用了 2 次父类构造函数，第一次是在 new Animal()，第二次是在 Animal.call() 这里。
+// 所以解决方案就是不直接调用父类构造函数给子类原型赋值，而是通过创建空函数 F 获取父类原型的副本。
+
