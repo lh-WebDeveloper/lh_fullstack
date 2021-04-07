b8d7d3d24b9f54c12b87b0e49eac597cb82f9085
lh19981109 <2727079972@qq.com>
2021-04-07 21:57:50 +0800

    vue-todolist

---
+++ vue/vue3-dome/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- <List/> -->
+  <todoList/>
+</template>
+<script>
+
+// import List from './components/list'
+ import todoList from './components/todoList'
+export default {
+  name: 'App',
+  components: {
+  todoList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
