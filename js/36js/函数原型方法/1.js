57f79f37febdc76729c111e0ea2237ec7dffbb4c
lh19981109 <2727079972@qq.com>
2021-04-10 20:57:06 +0800

    函数原型方法

---
+++ js/36js/函数原型方法/1.js
@@ -0,0 +1,14 @@
+//call方法
+Function.prototype.call2 = function (context) {
+    var context = context || window;
+    context.fn = this;
+
+    var args = [];
+    for(var i = 1, len = arguments.length; i < len; i++) {
+        args.push('arguments[' + i + ']');
+    }
+    var result = eval('context.fn(' + args +')');
+
+    delete context.fn
+    return result;
+}
