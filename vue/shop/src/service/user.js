a570eb71ec1040cd527612459fe1b3daa9d319e1
lh19981109 <2727079972@qq.com>
2021-04-18 16:55:01 +0800

    shop

---
+++ vue/shop/src/service/user.js
@@ -0,0 +1,8 @@
+import axios from '../utils/axios'
+
+export function register(params){
+    return axios.post('/user/register',params)
+}
+export function login(params){
+    return axios.post('/user/login',params)
+}
