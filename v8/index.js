81e7357fabc97e2d3ab7c07986dfa61bd19d73fd
lh19981109 <2727079972@qq.com>
2021-04-22 22:44:46 +0800

    v8

---
+++ v8/index.js
@@ -0,0 +1,27 @@
+// function foo(){
+//     var a=1
+//     var b=a
+//     a=2
+//     console.log(a);//2
+//     console.log(b);//1
+// }
+// foo()
+
+// function foo(){
+//     var a={name:'燕杰'}
+//     var b=a
+//     a.name='支总'
+//     console.log(a);//{ name: '支总' }
+//     console.log(b);//{ name: '支总' }
+// }
+// foo()
+
+// console.log(typeof(null))
+
+function foo(){
+    var a='燕杰';
+    var b=a;
+    var c={name:"燕杰"}
+    var d=c  
+}
+foo()
