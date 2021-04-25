f974db2df85cd8af7c63365588680c6912c91273
lh19981109 <2727079972@qq.com>
2021-04-25 16:09:16 +0800

    js执行机制

---
+++ js执行机制/index.js
@@ -0,0 +1,7 @@
+var myname=undefined
+function showName(){
+    console.log('队长的翘臀')
+}
+showName()
+console.log(myname);
+myname='队长'
---
+++ js执行机制/index3.js
@@ -0,0 +1,6 @@
+function bar(){
+
+}
+function foo(){
+    
+}
