2ad57cd4f0d1aad74c895dd23478d1da327ea9af
lh19981109 <2727079972@qq.com>
2021-04-26 22:20:51 +0800

    爬虫

---
+++ interview/fanye/master.js
@@ -0,0 +1,36 @@
+//fork
+//www.domain.com/path-> application n个
+//client http协议 -> ip地址 -> http应用层-> 80端口-> spider application
+//多个进程共享一个端口 
+const cluster=require('cluster');
+const cpuNums=require('os').cpus().length;
+cluster.setupMaster({
+    exec:'worker.js',
+    args:['--use','http']
+});
+console.log(`一共开启${cpuNums}个子进程来进行爬取`)
+console.log(`爬取数据是乱序的,因此给爬取的数据加movieIndex`)
+let pageNum=10;
+const startTime=Date.now();
+for(let i=0;i<cpuNums;i++){
+    let work=cluster.fork();
+    // 通信 IO操作 
+    // ngnix 
+    work.send([i,cpuNums,pageNum])
+    work.on('message',(msg)=>{
+        // console.log(msg,'----------');
+        pageNum--;
+        if(pageNum===0){
+            console.log(`\n已完成 所有爬取,using ${Date.now()-startTime} ms\n`)
+            console.log('关闭子进程');
+            cluster.disconnect();
+        }
+    })
+}
+
+cluster.on('fork',(worker)=>{
+    console.log(`[master]:fork worker ${worker.id}\n`);
+})
+cluster.on('exit',(worker)=>{
+    console.log(`[master]:子进程${worker.id}被关闭`);
+})
---
+++ interview/fanye/readme.md
@@ -0,0 +1,25 @@
+# 瑞幸咖啡  虎牙 猫眼 马峰窝 新浪微博 作业帮 哔哩哔哩 汽车之家 喜马拉雅 荔枝 小鹏汽车
+
+# 小米 金山 京东数科 快手
+
+# 6月初 美团 字节 
+
+
+# 分页
+项目中的特点:全栈的展示
+- sql limit offset
+- 前端也有分页 antd 列表组件
+
+- cluster cpu多核
+    n 0-(n-1) 
+- 前后端 协作 JSON 是数据格式和通信格式
+    API开发文档,可以在项目需求会后与后端 去确定
+    各司其职,可以使用mock/json-server
+    1. url
+    2. restful method params
+    3. text/html text application/json
+
+
+
+
+
