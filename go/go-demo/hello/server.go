d237a2747fe66b1f0953148e7887983295bb729f
lh19981109 <2727079972@qq.com>
2021-05-21 22:26:49 +0800

    go语言

---
+++ go/go-demo/hello/server.go
@@ -0,0 +1,9 @@
+package hello
+import "fmt"
+func Printf()  {
+	fmt.Println("public")
+}
+
+func printf()  {
+	fmt.Println("Hello Wrold")
+}
---
+++ go/go-demo/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+func main(){
+	// hello.Printf()
+	// var age = 18
+	// name :="lh"
+	// fmt.Println(age,name)
+	// for i:=0;i<10;i++{
+	// 		fmt.Println(i)
+	// }
+	fmt.Println()
+}
