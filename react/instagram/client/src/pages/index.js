024d2e589a13af0bc5e57c696f26143108ccafcc
lh19981109 <2727079972@qq.com>
2021-05-23 21:37:56 +0800

    instagram

---
+++ react/instagram/client/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '@comments/nav/index.js'
+class Instagram extends React.Component{
+    render(){
+        return (
+            <main>
+                <Nav/>
+            </main>
+        )
+    }
+}
+export default Instagram
