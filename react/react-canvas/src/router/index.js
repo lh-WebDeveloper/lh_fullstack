583830956ab71099acc2f522420b5ecec4c31037
lh19981109 <2727079972@qq.com>
2021-06-01 16:23:15 +0800

    黑客帝国

---
+++ react/react-canvas/src/router/index.js
@@ -0,0 +1,12 @@
+// src -> router -> index.js
+import loadable from '@loadable/component';
+
+const routes = [
+  {
+        path: '/hacker',
+        name: '黑客帝国',
+        component: loadable(() => import(/* webpackChunkName: 'hacker' */ '../views/Hacker')),
+    }
+];
+
+export default routes;
