d0ec466a7937466de35344071de66364779f2ee4
lh19981109 <2727079972@qq.com>
2021-06-05 09:52:40 +0800

    vue-demo-try

---
+++ vue/vuex-demo-try/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+  {{$store.state.count}}
+  {{evenOrOdd}}
+  <button @click="increment()">Increment</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import {mapActions, mapGetters} from 'vuex'
+export default {
+  name: 'App',
+  computed:mapGetters([
+    'evenOrOdd'
+  ]),
+  methods: mapActions([
+    'increment',
+    // 'decrement',
+    // 'incrementIfOdd',
+    // 'incrementAsync'
+  ]),
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
