a649921fe977918eb640f1a20f58457d2589d9d5
lh19981109 <2727079972@qq.com>
2021-06-27 16:10:18 +0800

    vue-waimai

---
+++ vue/vue-waimai/src/components/ratings/ratings.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="ratings" ref="ratings"></div>
+</template>
+<script>
+const ERR_OK=0;
+const response=require('../../common/data/ratings.json');
+export default {
+  data(){
+    return{
+      ratings:[]
+    }
+  },
+  created() {
+    if(response.errno===ERR_OK){
+      this.ratings=response.data
+      this.$nextTick(()=>{
+        this.scroll=new BScroll(this.$refs.ratings,{
+          click:true
+        })
+      })
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
